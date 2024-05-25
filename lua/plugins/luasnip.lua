bc26b1751e89fad5550e1155276764b09c9ac199
netbrum <netbrum@tutanota.com>
2024-05-25 21:04:55 +0200

    feat: add luasnip cmp completion

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,5 +1,19 @@
 return {
   "L3MON4D3/LuaSnip",
+  dependencies = {
+    "nvim-cmp",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+    },
+    opts = function(_, opts)
+      opts.snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      }
+      table.insert(opts.sources, { name = "luasnip" })
+    end,
+  },
   opts = {
     history = true,
     delete_check_events = "TextChanged",
