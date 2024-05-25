d8792c00360827ff4f4227d993fa05a32d1f1621
netbrum <netbrum@tutanota.com>
2024-05-25 20:33:19 +0200

    feat: add indent-blankline

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,18 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  event = { "LazyFile" },
+  opts = {
+    scope = { show_start = false, show_end = false },
+    exclude = {
+      filetypes = {
+        "help",
+        "dashboard",
+        "neo-tree",
+        "lazy",
+        "mason",
+        "toggleterm",
+      },
+    },
+  },
+  main = "ibl",
+}
