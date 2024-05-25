127b7fb8f8656ac96d20d8fb5ea791f0855d3a4b
netbrum <netbrum@tutanota.com>
2024-05-25 21:19:54 +0200

    feat: add ts-context-commentstring

--- lua/plugins/minicomment.lua
+++ lua/plugins/minicomment.lua
@@ -1,10 +1,24 @@
 return {
   "echasnovski/mini.comment",
   version = "*",
+  dependencies = {
+    {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+      lazy = true,
+      opts = {
+        enable_autocmd = false,
+      },
+    },
+  },
   opts = {
     mappings = {
       comment_line = "<leader>7",
       comment_visual = "<leader>7",
+    },
+    options = {
+      custom_commentstring = function()
+        return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+      end,
     },
   },
 }
