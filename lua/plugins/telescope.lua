bb2c35a85da1fb4cccfb380c41f3cd79cff1a658
netbrum <netbrum@tutanota.com>
2024-05-25 21:30:30 +0200

    feat: add telescope-fzf-native

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,33 @@
+local have_make = vim.fn.executable("make") == 1
+local have_cmake = vim.fn.executable("cmake") == 1
+
+local Utils = require("utils.load")
+
 return {
   "nvim-telescope/telescope.nvim",
   version = false,
   lazy = false,
   dependencies = {
     "nvim-lua/plenary.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = have_make and "make"
+          or "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+        enabled = have_make or have_cmake,
+        config = function(plugin)
+          Utils.on_load("telescope.nvim", function()
+            local ok = pcall(require("telescope").load_extension, "fzf")
+            if not ok then
+              local lib = plugin.dir .. "/build/libfzf.so"
+              if not vim.uv.fs_stat(lib) then
+                require("lazy").build({ plugins = { plugin }, show = false })
+              end
+            end
+          end)
+        end,
+      },
+    },
   },
   -- stylua: ignore
   keys = {
