b17008c8185bf8c5ebb3ec0833330f2a081b8a72
netbrum <netbrum@tutanota.com>
2024-12-20 21:38:07 +0100

    feat: update blink.cmp config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,34 +1,96 @@
-local icons = require("utils.icons")
-
 return {
   "saghen/blink.cmp",
-  lazy = false,
-  dependencies = "rafamadriz/friendly-snippets",
   version = "*",
-  opts = {
-    kind_icons = icons.kinds,
-    keymap = { preset = "enter" },
-    highlight = {
-      use_nvim_cmp_as_default = false,
+  opts_extend = {
+    "sources.completion.enabled_providers",
+    "sources.compat",
+    "sources.default",
+  },
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    {
+      "saghen/blink.compat",
+      optional = true,
+      opts = {},
+      version = "*",
     },
-    nerd_font_variant = "mono",
-    windows = {
-      autocomplete = {
-        winblend = vim.o.pumblend,
+  },
+  event = "InsertEnter",
+  opts = {
+    appearance = {
+      use_nvim_cmp_as_default = false,
+      nerd_font_variant = "mono",
+    },
+    completion = {
+      trigger = {
+        show_on_insert_on_trigger_character = false,
+      },
+      accept = {
+        auto_brackets = {
+          enabled = true,
+        },
+      },
+      menu = {
+        draw = {
+          treesitter = { "lsp" },
+        },
       },
       documentation = {
         auto_show = true,
+        auto_show_delay_ms = 200,
       },
       ghost_text = {
         enabled = vim.g.ai_cmp,
       },
     },
-    accept = { auto_brackets = { enabled = true } },
     sources = {
-      completion = {
-        enabled_providers = { "lsp", "path", "snippets" },
-      },
+      compat = {},
+      default = { "lsp", "path", "snippets", "buffer" },
+      cmdline = {},
+    },
+    keymap = {
+      preset = "enter",
+      ["<C-y>"] = { "select_and_accept" },
     },
   },
-  opts_extend = { "sources.completion.enabled_providers" },
+  ---@param opts blink.cmp.Config | { sources: { compat: string[] } }
+  config = function(_, opts)
+    local enabled = opts.sources.default
+    for _, source in ipairs(opts.sources.compat or {}) do
+      opts.sources.providers[source] = vim.tbl_deep_extend(
+        "force",
+        { name = source, module = "blink.compat.source" },
+        opts.sources.providers[source] or {}
+      )
+      if type(enabled) == "table" and not vim.tbl_contains(enabled, source) then
+        table.insert(enabled, source)
+      end
+    end
+
+    opts.sources.compat = nil
+
+    for _, provider in pairs(opts.sources.providers or {}) do
+      ---@cast provider blink.cmp.SourceProviderConfig|{kind?:string}
+      if provider.kind then
+        local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+        local kind_idx = #CompletionItemKind + 1
+
+        CompletionItemKind[kind_idx] = provider.kind
+        CompletionItemKind[provider.kind] = kind_idx
+
+        local transform_items = provider.transform_items
+        provider.transform_items = function(ctx, items)
+          items = transform_items and transform_items(ctx, items) or items
+          for _, item in ipairs(items) do
+            item.kind = kind_idx or item.kind
+          end
+          return items
+        end
+
+        provider.kind = nil
+      end
+    end
+
+    require("blink.cmp").setup(opts)
+  end,
 }
